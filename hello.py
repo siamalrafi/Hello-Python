07fb9596829521b6aabb57d3aeb55a7a1b28df8a
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-01-29 23:40:14 +0600

    This isthe nexs

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print('This is the default configuration for the application. This configuration we will use to configure the application. This is  the')
+print('This is the default configuration for the applicaiotn')
