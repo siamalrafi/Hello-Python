2ddb9a3e52424718b73e0a20dcc2a70fe85e8f60
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-01 23:52:07 +0600

    Thsi is the problem

---
+++ src/problem.py
@@ -0,0 +1,37 @@
+# Exercise 1: Print First 10 natural numbers using while loop
+""" i = 1
+while i < 11:
+    print(i)
+    i += 1 """
+
+
+#  Exercise 2: print this pattern
+""" for x in range(1, 6, 1):
+    for y in range(1, x+1):
+        print(y, end=' ')
+    print('') """
+
+# Exercise 3: Print all sum of 0 to 10
+
+""" sum = 0
+for x in range(11):
+    sum += x
+print('Total sum:', sum) """
+
+""" sum = 0
+num = int(input('Enter a number: '))
+for x in range(1, num+1, 1):
+    sum += x
+print('Total sum:', sum)
+ """
+
+
+
+
+
+
+
+
+
+
+
