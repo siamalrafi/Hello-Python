7ae883e77c3a896f6c9cc3ec918b4d29031866d5
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-03 14:36:28 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
-def myfun(a=1, b=4):
-    return a+b
-
-
-print(myfun(a=55))
+n = 15
+m = 7
+ans1 = n+m
+print("Addition of",n,"and",m,"is", ans1)
+ans2 = n-m
+print("Subtraction of",n,"and",m,"is", ans2)
+ans3 = n*m
+print("Multiplication of",n,"and",m,"is", ans3)
+ans4 = n/m
+print("Division of",n,"and",m,"is", ans4)
+ans5 = n%m
+print("Modulus of",n,"and",m,"is", ans5)
+ans6 = n//m
+print("Floor Division of",n,"and",m,"is", ans6)
---
+++ src/function.py
@@ -0,0 +1,38 @@
+# Exercise 1: Create a function in Python
+""" def demo(name, age):
+    print(name, age)
+demo("Ben", 25)
+ """
+
+
+# Exercise 2: Create a function with variable length of arguments
+""" def func1(*numbers):
+    for x in numbers:
+        print(x) 
+func1(10, 20, 30) """
+
+
+# Exercise 3: Return multiple values from a function
+""" def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+    return addition, subtraction
+print(calculation(40, 10)) """
+
+# Exercise 8: Generate a Python list of all the even numbers between 4 to 30
+""" print(list(range(4, 30, 2)))
+ """
+
+# Exercise 9: Find the largest item from a given list
+
+""" x = [4, 6, 8, 24, 300, 12, 2]
+print(min(x))
+print(max(x))
+ """
+
+
+
+
+
+
+ 
