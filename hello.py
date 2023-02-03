f18df650395253885b6d1b0ae13a3c9ec329ee39
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-03 15:28:18 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -1,2 +1,1 @@
-str2 = "Silver Spoon"
-print(str2.split(" "))  # Splits the string at the whitespace " ".
+ 
---
+++ src/stringPr.py
@@ -0,0 +1,19 @@
+# Exercise 1A: Create a string made of the first, middle and last character
+
+
+""" string = 'nmenw'
+
+firstC = string[0]
+middleC = string[int(len(string) / 2)]
+lastC = string[-1:]
+
+newWord = firstC+middleC+lastC
+
+print('New word: ' + newWord) """
+
+
+
+
+
+
+
