769c7c00adce04343d951803290de69d85cb0933
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-08 14:56:25 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -1,3 +1,69 @@
-git add .
-git commit -m"This is the python story"
-git push
+freecodecamp = "freeCodeCamp"
+
+>>> freecodecamp.capitalize()
+'Freecodecamp'
+
+>>> freecodecamp.count("C")
+2
+
+>>> freecodecamp.find("e")
+2
+
+>>> freecodecamp.index("p")
+11
+
+>>> freecodecamp.isalnum()
+True
+
+>>> freecodecamp.isalpha()
+True
+
+>>> freecodecamp.isdecimal()
+False
+
+>>> freecodecamp.isdigit()
+False
+
+>>> freecodecamp.isidentifier()
+True
+
+>>> freecodecamp.islower()
+False
+
+>>> freecodecamp.isnumeric()
+False
+
+>>> freecodecamp.isprintable()
+True
+
+>>> freecodecamp.isspace()
+False
+
+>>> freecodecamp.istitle()
+False
+
+>>> freecodecamp.isupper()
+False
+
+>>> freecodecamp.lower()
+'freecodecamp'
+
+>>> freecodecamp.lstrip("f")
+'reeCodeCamp'
+
+>>> freecodecamp.rstrip("p")
+'freeCodeCam'
+
+>>> freecodecamp.replace("e", "a")
+'fraaCodaCamp'
+
+>>> freecodecamp.split("C")
+['free', 'ode', 'amp']
+
+>>> freecodecamp.swapcase()
+'FREEcODEcAMP'
+
+>>> freecodecamp.title()
+'Freecodecamp'
+
+>>> freecodecamp.upper()
