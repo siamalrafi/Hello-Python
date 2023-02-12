85458ad6ccd8297762e1d626d10e4aaf8525b661
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-12 23:27:35 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
 fruits = ("apple", "banana", "cherry")
 mytuple = fruits * 10
 
+git add .
+git commit -m"This is the python story"
+git pushgit add .
+git commit -m"This is the python story"
+git pushgit add .
+git commit -m"This is the python story"
+git pushgit add .
+git commit -m"This is the python story"
+git pushgit add .
+git commit -m"This is the python story"
+git push
