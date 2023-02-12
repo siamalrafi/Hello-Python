77812a889f42eb5dd54aa2766ccf61536844e1c5
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-12 16:09:12 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -3,4 +3,3 @@ print('this is the python code. Python is the king')
 print('this is the python code. Python is the king')
 print('this is the python code. Python is the king')
 print('this is the python code. Python is the king')
-print('
