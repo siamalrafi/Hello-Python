ebef4f9d8899d5ea4957a9d5e2d2bc58194583ae
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-12 16:08:29 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ print('this is the python code. Python is the king')
 print('this is the python code. Python is the king')
 print('this is the python code. Python is the king')
 print('this is the python code. Python is git add .
-git commit -m"This is the python story"
+git commit -m"This is the 
