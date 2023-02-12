21c17956c8e38123606d411bd0cc3e96eaf51691
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-12 23:30:10 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -1,12 +1,16 @@
-fruits = ("apple", "banana", "cherry")
-mytuple = fruits * 10
+fruits = (1, 4332, 3, 4, 5)
+x = list(fruits)
+x.sort()
+x = tuple(x)
+print(x)
+print(type(x))
 
-git add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
+# git add .
+# git commit -m"This is the python story"
+# git pushgit add .
+# git commit -m"This is the python story"
+# git pushgit add .
+# git commit -m"This is the python story"
+# git pushgit add .
+# git commit -m"This is the python story"
+# git pushgit add .
