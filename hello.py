734173e3bd266eb8cdaae7ddcc0b8ab658a1ab13
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2023-02-13 19:23:23 +0600

    This is the python story

--- hello.py
+++ hello.py
@@ -35,23 +35,3 @@ git pushgit add .
 git commit -m"This is the python story"
 git pushgit add .
 git commit -m"This is the python story"
-git push
-git add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git pushgit add .
-git commit -m"This is the python story"
-git push
